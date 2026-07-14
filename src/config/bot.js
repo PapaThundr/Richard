3b7ebed1a24b1db5cf78cc98887f5df17200eb3d
PapaThundr <jcarrasco007@yahoo.com>
2026-07-13 22:07:33 -0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -93,8 +93,8 @@ export const botConfig = {
   embeds: {
     colors: {
       // Main brand colors.
-      primary: "#CFDFE6",
-      secondary: "#2F3136",
+      primary: "#0077B6",
+      secondary: "#ADE8F4",
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287",
